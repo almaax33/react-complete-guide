f05dfb97a6a2b91288cca16a2f88396be9eeac57
Alma Garcia <lin_alma30@hotmail.com>
2021-08-14 16:42:19 -0400

    Add my routing practice

---
+++ my-practice/routing/src/App.js
@@ -0,0 +1,34 @@
+import { Redirect, Route, Switch } from "react-router-dom";
+
+import NewQuote from "./pages/NewQuote";
+import QuoteDetails from "./pages/QuoteDetails";
+import AllQuotes from "./pages/AllQuotes";
+
+import Layout from "./components/layout/Layout";
+import NotFound from "./pages/NotFound";
+
+function App() {
+  return (
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/quotes" />
+        </Route>
+        <Route path="/quotes/:quoteId">
+          <QuoteDetails />
+        </Route>
+        <Route path="/quotes" exact>
+          <AllQuotes />
+        </Route>
+        <Route path="/new-quote">
+          <NewQuote />
+        </Route>
+        <Route path="*">
+          <NotFound />
+        </Route>
+      </Switch>
+    </Layout>
+  );
+}
+
+export default App;
---
+++ my-practice/routing/src/pages/NotFound.js
@@ -0,0 +1,8 @@
+const NotFound = () => {
+  return (
+    <div className="centered">
+      <p>Not found 404</p>
+    </div>
+  );
+};
+export default NotFound;
